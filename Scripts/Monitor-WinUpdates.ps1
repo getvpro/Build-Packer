be43f478f4f268c37522bed0180e4ee36e165061
Owen Reynolds <50507806+getvpro@users.noreply.github.com>
2021-12-05 18:10:41 -0500

    Add files via upload

--- Scripts/Monitor-WinUpdates.ps1
+++ Scripts/Monitor-WinUpdates.ps1
@@ -1,4 +1,81 @@
-﻿Function Test-PendingReboot {
+﻿<#
+
+.FUNCTIONALITY
+Checks for status of windows updates started by Start-WinUpdates.ps1 script: UsoClient.exe StartInteractiveScan
+When no updates are left, the related scheduled tasks will be disabled
+
+.SYNOPSIS
+Checks for status of windows updates started by Start-WinUpdates.ps1 script: UsoClient.exe StartInteractiveScan
+When no updates are left, the related scheduled tasks will be disabled
+
+
+.NOTES
+Change log
+
+Dec 4, 2021
+-Initial version
+
+Dec 5, 2021
+-Added logging and module logic from Get-WinUpdatesPacker.ps1 script
+
+
+.DESCRIPTION
+Author oreynolds@gmail.com
+
+.EXAMPLE
+./Monitor-WinUpdates.ps1
+
+.NOTES
+
+.Link
+N/A
+
+#>
+
+## Variables
+
+$LogTimeStamp = (Get-Date).ToString('MM-dd-yyyy-hhmm-tt')
+$ScriptLog = (Get-ChildItem C:\Admin\Build | Sort-Object -Property LastWriteTime | Where-object {$_.Name -like "WinPackerBuild*"} | Select -first 1).FullName
+
+### Functions
+
+Function Write-CustomLog {
+    Param(
+    [String]$ScriptLog,    
+    [String]$Message,
+    [String]$Level
+    
+    )
+
+    switch ($Level) { 
+        'Error' 
+            {
+            $LevelText = 'ERROR:' 
+            $Message = "$(Get-Date): $LevelText Ran from $Env:computername by $($Env:Username): $Message"
+            Write-host $Message -ForegroundColor RED            
+            } 
+        
+        'Warn'
+            { 
+            $LevelText = 'WARNING:' 
+            $Message = "$(Get-Date): $LevelText Ran from $Env:computername by $($Env:Username): $Message"
+            Write-host $Message -ForegroundColor YELLOW            
+            } 
+
+        'Info'
+            { 
+            $LevelText = 'INFO:' 
+            $Message = "$(Get-Date): $LevelText Ran from $Env:computername by $($Env:Username): $Message"
+            Write-host $Message -ForegroundColor GREEN            
+            } 
+
+        }
+        
+        Add-content -value "$Message" -Path $ScriptLog
+}
+
+
+Function Test-PendingReboot {
     if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
     if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
@@ -14,22 +91,58 @@
     return $false
 }
 
+###
+
+IF (!(Get-PackageProvider -ListAvailable nuget) ) {
+
+    Install-PackageProvider -Name NuGet -Force
+    Write-CustomLog -ScriptLog $ScriptLog -Message "The Nuget package manager will be installed" -Level INFO
+    Write-EventLog -LogName SYSTEM -Source $EventIDSrc -EventId 0 -EntryType INFO -Message "The Nuget package manager will be installed"
+
+}
+
+IF (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {
+
+    Install-module pswindowsupdate -force -AllowClobber
+    Write-CustomLog -ScriptLog $ScriptLog -Message "The PSWindowsUpdate module will be installed" -Level INFO
+}
+
+###
+
+Import-Module -Name PSWindowsUpdate
+
+$Updates = Get-WUList
+
+If ($Updates -eq $Nul) {
+
+    Write-CustomLog -ScriptLog $ScriptLog -Message "no windows updates to apply, start/monitor windows update tasks will be disabled and the script will exit" -Level INFO
+    Get-ScheduledTask -TaskName "*WinUpdates*" | Disable-ScheduledTask
+    EXIT
+
+}
+
+Write-CustomLog -ScriptLog $ScriptLog -Message "The following windows updates will be installed: `n $($Updates | Out-String)" -Level INFO    
+
 write-host "Launch windows update UI"
 
 control update
 
 Do {
 
-    Write-host "Check for pending restart, sleep for 30 seconds"
+    Write-host "Check for windows update status, sleep for 30 seconds" -ForegroundColor cyan
     Start-Sleep -s 30
     $aa = Test-PendingReboot
-    $bb = get-service -Name BITS | Select-Object -ExpandProperty Status
+    $bb = get-service -Name msiserver | Select-Object -ExpandProperty Status
+    #$bb = get-service -Name BITS | Select-Object -ExpandProperty Status
+    #$bb = Get-WUInstallerStatus | Select-Object -ExpandProperty IsBusy
 }
 
 Until ($aa -eq $True -and $bb -eq "Stopped") 
 
-write-warning "Reboot detected, rebooting in 30 seconds"
-start-sleep -s 30
+Write-CustomLog -ScriptLog $ScriptLog -Message "Windows updates have finished processing, machine will be rebooted in 60 seconds" -Level INFO
+
+start-sleep -s 60
+
 restart-computer -force
 
     
--- Scripts/Start-WinUpdates.ps1
+++ Scripts/Start-WinUpdates.ps1
@@ -3,4 +3,3 @@
 UsoClient.exe StartInteractiveScan
 
 add-content -Path C:\Admin\Build\WindowsUpdates.txt -Value "$(Get-Date): Windows update via UsoClient.exe started"
-
