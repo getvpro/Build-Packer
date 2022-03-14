e45f9b11c712b30c522e6eb3b3af4686bd4b8865
Owen Reynolds <50507806+getvpro@users.noreply.github.com>
2022-03-14 10:59:10 -0400

    Add files via upload

--- Scripts/Start-WinUpdates.ps1
+++ Scripts/Start-WinUpdates.ps1
@@ -4,6 +4,9 @@
 ### March 13, 2022
 ## #control update added, but disabled
 
+### March 14, 2022
+# Added ms-settings:windowsupdate-action, not used, saved for future use as Usoclient.exe is legacy
+
 Get-ScheduledTask -TaskName MicrosoftEdgeUpdateTaskMachine* -ErrorAction SilentlyContinue | Stop-ScheduledTask
 Get-ScheduledTask -TaskName MicrosoftEdgeUpdateTaskMachine* -ErrorAction SilentlyContinue | Disable-ScheduledTask
 
@@ -11,6 +14,7 @@ Set-location "C:\Windows\System32"
 
 UsoClient.exe StartInteractiveScan
 
-#control update 
+# control update 
+## ms-settings:windowsupdate-action
 
 add-content -Path C:\Admin\Build\WindowsUpdates.txt -Value "$(Get-Date): Windows update via UsoClient.exe started"
