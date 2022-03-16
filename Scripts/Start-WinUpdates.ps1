7f4eb91d86e97f79a22b3125cd2d64b3a7c2618d
Owen Reynolds <50507806+getvpro@users.noreply.github.com>
2022-03-15 23:42:25 -0400

    Add files via upload

--- Scripts/Start-WinUpdates.ps1
+++ Scripts/Start-WinUpdates.ps1
@@ -6,15 +6,26 @@
 
 ### March 14, 2022
 # Added ms-settings:windowsupdate-action, not used, saved for future use as Usoclient.exe is legacy
+# https://www.urtech.ca/2018/11/usoclient-documentation-switches/
+
+#March 15, 2022
+# ServiceUI used to launch windows update
 
 Get-ScheduledTask -TaskName MicrosoftEdgeUpdateTaskMachine* -ErrorAction SilentlyContinue | Stop-ScheduledTask
 Get-ScheduledTask -TaskName MicrosoftEdgeUpdateTaskMachine* -ErrorAction SilentlyContinue | Disable-ScheduledTask
 
 Set-location "C:\Windows\System32"
 
-UsoClient.exe StartInteractiveScan
+c:\Windows\system32\ServiceUI.exe -process:explorer.exe "c:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -WindowStyle minimized -Executionpolicy bypass -Command "UsoClient.exe ScanInstallWait"
 
-# control update 
-## ms-settings:windowsupdate-action
+c:\Windows\system32\ServiceUI.exe -process:explorer.exe "c:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -WindowStyle minimized -Executionpolicy bypass -Command "UsoClient.exe StartDownload"
+
+c:\Windows\system32\ServiceUI.exe -process:explorer.exe "c:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -WindowStyle minimized -Executionpolicy bypass -Command "UsoClient.exe Startinstall"
+
+c:\Windows\system32\ServiceUI.exe -process:explorer.exe "c:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -WindowStyle minimized -Executionpolicy bypass -Command "explorer ms-settings:windowsupdate-action"
+
+start-sleep -Seconds 10
+
+c:\Windows\system32\ServiceUI.exe -process:explorer.exe "c:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" -WindowStyle minimized -Executionpolicy bypass -Command "explorer ms-settings:windowsupdate-action"
 
 add-content -Path C:\Admin\Build\WindowsUpdates.txt -Value "$(Get-Date): Windows update via UsoClient.exe started"
